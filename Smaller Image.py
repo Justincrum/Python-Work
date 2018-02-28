b92e3420cbbe9fdd785e29ed4258970057b8eade
Justin Crum <jcrum@email.arizona.edu>
2018-02-28 09:17:34 -0700

    Normalization and Color Bar
    Added normalization abilities to the plot and added a color bar.

--- Smaller Image.py
+++ Smaller Image.py
@@ -7,19 +7,25 @@ Created on Fri Feb 09 09:12:47 2018
 #This code is for applying the fractional laplacian operator to a smaller image.
 import numpy as np
 from numpy import linalg as LA
-from PIL import Image
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-from numpy import *
 from scipy import sparse as SP
 from datetime import datetime
-import matplotlib.cm as cm
-from matplotlib.colors import Normalize
-
 
 startTime = datetime.now()
 
-img=mpimg.imread('stinkbugssmall.png')  #This block reads the image and converts to grayscale.
+#gray = np.zeros((10,10))
+#for i in range (0,10):
+#    for j in range(1,9):
+#        if i > 0:
+#            gray[i,j]=1
+#        if i == 9:
+#            gray[i,j]=0
+            
+#imgplt = plt.imshow(gray, cmap = plt.get_cmap('gray_r'))            
+            
+
+img=mpimg.imread('stinkbugsmall.png')  #This block reads the image and converts to grayscale.
 r, g, b =img[:,:,0], img[:,:,1], img[:,:,2]
 gray = 20.0/61*r + 40.0/61*g +1.0/61*b 
 n, m = np.shape(gray) #n rows, m columns
@@ -64,7 +70,7 @@ C = LA.inv(V)
 #Setting up an array to make a color map.
 fractionals = []
 
-for i in range(0,6):
+for i in range(0,12):
     T=S
     T=T**(i/5.0)
     T = np.diag(T)
@@ -74,15 +80,21 @@ for i in range(0,6):
     Laps = np.reshape(Laps,(n,m)) 
     #Storing each of the instances of the fractional laplacian.
     fractionals.append(Laps)
+    
 #Creating the right normalization factor for the fractional laplacians to plot.   
 small = np.min(fractionals)
 large = np.max(fractionals)
-#norm = Normalize(vmin=small, vmax=large, clip=False)
-for i in range(0,6):
+
+for i in range(0,12):
     nLaps = fractionals[i]
-    fig.add_subplot(2,3,i+1)
-    imgplt = plt.imshow(nLaps, cmap = plt.get_cmap('gray'),vmin=small, vmax=large) 
+    fig.add_subplot(3,4,i+1)
+    #Uncomment one of the next two lines for the code to plot.  The first normalizes.
+    imgplt = plt.imshow(nLaps, cmap = plt.get_cmap('gray_r'),vmin=small, vmax=large) 
+    #imgplt = plt.imshow(nLaps, cmap = plt.get_cmap('gray_r'))
     #Plotting the images.
+fig.subplots_adjust(right=0.8)
+cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+fig.colorbar(imgplt, cax=cbar_ax)
 
 #The Below works, but is kind of uninspired.
 
